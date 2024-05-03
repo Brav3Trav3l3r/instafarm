b00bcbdc046bdd9a4cf96937236ae415e1d408e7
Yash Gajbhiye <yashgajbhiye10@gmail.com>
2024-05-03 11:26:14 +0530

    prepare to deploy

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,13 +1,8 @@
-import express from 'express'
 import dotenvFlow from 'dotenv-flow';
 dotenvFlow.config();
+import app from '../app';
 
-const app = express();
 const port = process.env.PORT;
-
-app.get("/", (req, res) => {
-  res.send("Express + TypeScript Server!!!!");
-});
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
