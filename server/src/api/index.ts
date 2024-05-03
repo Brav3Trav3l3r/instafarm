14b7aeaada641bde8452d5e2f5019c52ab61b6b5
Yash Gajbhiye <yashgajbhiye10@gmail.com>
2024-05-03 12:57:23 +0530

    init frontend

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -2,8 +2,22 @@ import dotenvFlow from 'dotenv-flow';
 dotenvFlow.config();
 import app from '../app';
 
+process.on('uncaughtException', (err: Error) => {
+  console.log('💥 uncaughtException', err.name, "-", err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
 const port = process.env.PORT;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+process.on('unhandledRejection', (err: Error) => {
+  console.log('💥 unhandledRejection:', err.name, "-", err.message);
+  // server.close(() => {
+  //   process.exit(1);
+  // });
 });
